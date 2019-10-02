ff09ce03e0c1a62f69121f739ac1d14e066edff3
Natalie Dabrowska <info@nataliedabrowska.com>
2019-10-02 13:51:51 +0800

    Add Icon

--- src/views/ContactPage/ContactPage.jsx
+++ src/views/ContactPage/ContactPage.jsx
@@ -10,8 +10,15 @@ class ContactPage extends React.Component {
         <Header />
         <div className="contact">
           <div className="contact-title">Kontakt</div>
+          <i class="fa fa-home icon-home" aria-hidden="true"></i>
           <div className="contact-information">
             Schronisko dla bezdomnych psów
+            <article>ul.polna 20 Morena</article>
+            <article>01-100 Wrocław</article>
+            <i class="fa fa-envelope-o icon-email" aria-hidden="true"></i>
+            <article>schronisko@gmail.com</article>
+            <i class="fa fa-phone icon-phone" aria-hidden="true"></i>
+            <article> +78 456 564 466</article>
           </div>
         </div>
 
