f671a5ea79808bd04b4b89d3c32f8af06a8151b6
Natalie Dabrowska <info@nataliedabrowska.com>
2019-12-18 12:04:20 +0700

    dog page fixed.

--- src/views/DogPage/DogPage.jsx
+++ src/views/DogPage/DogPage.jsx
@@ -1,20 +1,36 @@
 import React from "react";
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
-import dogs from "../../api/data";
+import api from "../../api/api";
 import "./style.css";
 
 class DogPage extends React.Component {
+  state = {
+    dogs: []
+  };
+
+  componentDidMount() {
+    api.getDogs().then(response => {
+      this.setState({
+        dogs: response
+      });
+    });
+  }
+
   render() {
     const id = this.props.match.params.id;
-    const singleDog = dogs.find(dog => dog.id === id);
+    const singleDog =
+      this.state.dogs.find(dog => {
+        return dog.id.toString() === id;
+      }) || {};
+    console.log(singleDog);
     return (
       <div className="app">
         <Header />
         <div className="dog-background">
           <div className="dog-page-name">{singleDog.name}</div>
           <div className="about-dog">
-            <div className="text-dog">{singleDog.information}</div>
+            <div className="text-dog">{singleDog.description}</div>
             <img className="dog-picture" src={singleDog.photo} alt="images" />
           </div>
         </div>
